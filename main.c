85bb7f4f3e4379029d719d703c7778044ed30f34
Jaemin Jeong <jaemjeon@c8r6s12.42seoul.kr>
2022-10-24 22:01:50 +0900

    restart pipex..

---
+++ main.c
@@ -0,0 +1,85 @@
+#include "pipex.h"
+
+void	ft_error(char *message, int exit_status)
+{
+	write(2, message, strlen(message));
+	write(2, "\n", 1);
+	exit(exit_status);
+}
+
+void	parsing(t_info *info, int argc, char *argv[])
+{
+	int	i, j;
+
+	i = 0;
+	if (argc < 5)
+		ft_error("argc error", 1);
+	while ((argc > i * 2 + 1) && strcmp(argv[2 * i + 1], "here_doc") == 0)
+		i++;
+	info->heredoc_count = i;
+	info->limiter = (char **)calloc(sizeof(char *), info->heredoc_count + 1);
+	i = 0;
+	while (i < info->heredoc_count)
+	{
+		info->limiter[i] = argv[2 * (i + 1)];
+		i++;
+	}
+	if (info->heredoc_count == 0)
+	{
+		info->infile_name = argv[1];
+		info->infile = open(info->infile_name, O_RDONLY);
+		if (info->infile == 0)
+			ft_error("error open infile", 1);
+	}
+	else
+	{
+		info->infile_name = argv[info->heredoc_count * 2 + 1];
+		info->infile = open(info->infile_name, O_RDONLY);
+		if (info->infile == 0)
+			ft_error("error open infile", 1);
+	}
+	i = info->heredoc_count * 2 + 1;
+	j = 0;
+	info->commands = (char **)calloc(sizeof(char *), argc - i + 1);
+	while (i < argc - 1)
+	{
+		info->commands[j] = argv[i];
+		i++;
+		j++;
+	}
+	info->outfile_name = argv[argc - 1];
+	info->outfile = open(info->outfile_name, O_RDONLY);
+	if (info->outfile == 0)
+		ft_error("error open outfile", 1);
+}
+
+void	print_info(t_info *info)
+{
+	int	i;
+
+	i = 0;
+	printf("commands :\n");
+	while (info->commands[i])
+	{
+		printf("[%d] : %s\n", i, info->commands[i]);
+		i++;
+	}
+	i = 0;
+	printf("heredoc_count : %d\n", info->heredoc_count);
+	while (info->limiter[i])
+	{
+		printf("[%d] : %s\n", i, info->limiter[i]);
+		i++;
+	}
+	printf("infile : [name] = %s, [fd] = %d\n", info->infile_name, info->infile);
+	printf("outfile : [name] = %s, [fd] = %d\n", info->outfile_name, info->outfile);
+}
+
+int main(int argc, char *argv[])
+{
+	t_info	info;
+	memset(&info, 0 , sizeof(t_info));
+	parsing(&info, argc, argv);
+	print_info(&info);
+	return (0);
+}
---
+++ pipex.h
@@ -0,0 +1,17 @@
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+typedef	struct s_info
+{
+	char	**commands;
+	int		heredoc_count;
+	char	**limiter;
+	char	*infile_name;
+	char	*outfile_name;
+	int		infile;
+	int		outfile;
+}	t_info;
